55ffe31cfd45aabd57a6da044d95cb6d54aae262
Yoann Fleury <yoann.fleury@yahoo.com>
2021-06-11 12:28:23 +0200

    fix: update code based on dependency update

--- src/main.rs
+++ src/main.rs
@@ -8,13 +8,16 @@ use structopt::StructOpt;
 extern crate log;
 
 fn main() {
-    // Get the command line options
+    // Get the command line options.
     let opt = Opt::from_args();
 
-    Logger::with_str(if opt.verbose { "debug" } else { "info" })
+    // Enable the logger with the output mode based on the given option.
+    Logger::try_with_str(if opt.verbose { "debug" } else { "info" })
+        .unwrap()
         .start()
         .unwrap_or_else(|e| panic!("Logger initialization failed with {}", e));
 
+    // Run the program using the given options.
     match run(opt) {
         Ok(_) => (),
         Err(err) => {
